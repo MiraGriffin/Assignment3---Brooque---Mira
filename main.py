3889a5ce4f36f9f27c942e6368d23a29d6f4fd86
brooque <bsteph04@calpoly.edu>
2026-02-09 12:38:14 -0800

    data def, tree_lt, starting length function

--- main.py
+++ main.py
@@ -16,6 +16,29 @@ def cnt_freq(text : str) -> List[int]:
     return freq
 
 
+@dataclass(frozen=True)
+class HLeaf:
+	count: int
+	char: str
+
+@dataclass(frozen=True)      
+class HNode:
+    count: int
+    char: str
+    left: HTree
+    right: HTree
+
+HTree : TypeAlias = Union [None, HLeaf]
+HTLTree: TypeAlias = Union[None, HNode]
+
+
+def tree_lt(tree_1: HTree, tree_2: HTree) -> bool:
+       
+       if tree_1.count < tree_2.count or (tree_1.count == tree_2.count and tree_1.char < tree_2.char):
+		        return True
+
+
+
 
 class Tests(unittest.TestCase):
     text1 : str = "aaa"
@@ -31,6 +54,9 @@ class Tests(unittest.TestCase):
         self.assertEqual(cnt_freq(self.text3)[65:70],
                          [1, 1, 1, 1, 0])
         self.assertEqual(cnt_freq(self.text4), [0] * 256)
+
+
+
 
 
 
