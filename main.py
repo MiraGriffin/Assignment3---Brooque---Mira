d79e81471d2e2e2f483664bab82f5e08015b945b
Mira Griffin <mirasgriffin@gmail.com>
2026-02-13 10:13:53 -0800

    finished 6

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ def cnt_freq(text : str) -> List[int]:
     return freq
 
 # Data Definitions
-HTree : TypeAlias = Union ['HNode', 'HLeaf']
-HTList : TypeAlias = Union['HTree', 'HTLNode']
-
 @dataclass(frozen=True)
 class HLeaf:
 	count: int
@@ -34,6 +31,9 @@ class HNode:
 class HTLNode:
     tree : HTree
     next : HTList
+
+HTree : TypeAlias = Union [HNode, HLeaf]
+HTList : TypeAlias = Union[HTree, HTLNode]
 
 # Huffman Tree Functions
 def tree_lt(tree_1: HTree, tree_2: HTree) -> bool:
